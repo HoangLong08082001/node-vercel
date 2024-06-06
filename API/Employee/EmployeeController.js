5ef193802bb806ec97ead8cfa5f50f9aa52ec027
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-06-06 11:05:42 +0700

    upload some module of employee

--- API/Employee/EmployeeController.js
+++ API/Employee/EmployeeController.js
@@ -5,8 +5,24 @@ const {
 } = require("../../middleware/JwtAction.js");
 const { ServiceEmployee } = require("./EmployeeModal.js");
 const bcrypt = require("bcrypt");
-const salt = 10;
+const nodemailer = require("nodemailer");
 
+const salt = 10;
+const randomNumberCodeVerfify = () => {
+  return Math.floor(100000 + Math.random() * 900000);
+};
+const getToday = () => {
+  const today = new Date();
+
+  // Lấy năm, tháng, và ngày từ đối tượng ngày
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Tháng bắt đầu từ 0 nên cần +1 và đảm bảo 2 chữ số
+  const day = String(today.getDate()).padStart(2, "0"); // Đảm bảo 2 chữ số
+
+  // Tạo chuỗi ngày theo định dạng yyyy-mm-dd
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+};
 //id_department	username	password	phone	status	code_verify	date_create	time_create
 const createEmployee = (req, res) => {
   let username = req.body.username;
@@ -27,7 +43,7 @@ const createEmployee = (req, res) => {
         if (hash) {
           pool.query(
             ServiceEmployee.create,
-            [id_department, username, hash, phone, 1],
+            [id_department, username, hash, phone, 1, getToday()],
             (err, data) => {
               if (err) {
                 throw er;
@@ -96,7 +112,7 @@ const getAllEmployee = (req, res) => {
         throw err;
       }
       if (data) {
-        return res.status(200).json({ result: data });
+        console.log(data);
       }
     });
   } catch (error) {
@@ -104,4 +120,139 @@ const getAllEmployee = (req, res) => {
   }
 };
 
-module.exports = { createEmployee, loginEmployee, getAllEmployee };
+const rePassword = (req, res) => {
+  let username = req.body.email;
+  let newpass = "";
+  console.log(username);
+  try {
+    pool.query(
+      ServiceEmployee.checkLogin,
+      [username, username],
+      (err, data) => {
+        if (err) {
+          throw err;
+        }
+        if (data.length > 0) {
+          newpass = randomNumberCodeVerfify();
+          bcrypt.hash(newpass.toString(), salt, (err, hash) => {
+            if (err) {
+              throw err;
+            }
+            if (hash) {
+              pool.query(
+                ServiceEmployee.rePass,
+                [hash, username],
+                (err, result) => {
+                  if (err) {
+                    throw err;
+                  }
+                  if (result) {
+                    const transport = nodemailer.createTransport({
+                      host: "smtp.gmail.com",
+                      port: 587,
+                      service: "gmail",
+                      secure: false,
+                      auth: {
+                        user: "longhoang882001@gmail.com",
+                        pass: "dyygjdykverudrtb",
+                      },
+                    });
+
+                    // Thiết lập email options
+                    const mailOptions = {
+                      from: "longhoang882001@gmail.com", // Địa chỉ email của người gửi
+                      to: `${username}`, // Địa chỉ email của người nhận
+                      subject: "Ecoop send new password", // Tiêu đề email
+                      text: `Send mail from Ecoop to renew password: ${newpass}`, // Nội dung email
+                    };
+                    transport.sendMail(mailOptions, (error, info) => {
+                      if (error) {
+                        throw error;
+                      }
+                      console.log("Verify code from Ecoop: " + info.response);
+                    });
+                    return res.status(200).json({ message: "success" });
+                  }
+                }
+              );
+            }
+          });
+        } else {
+          return res.status(400).json({ message: "Email này không tồn tại" });
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({ message: "fails" });
+  }
+};
+
+const setNewPassword = (req, res) => {
+  let username = req.body.email;
+  let oldPassowrd = req.body.oldPassword;
+  let newPassword = req.body.newPassword;
+  console.log(username + " " + oldPassowrd + " " + newPassword);
+  try {
+    pool.query(
+      ServiceEmployee.checkUsernamePassword,
+      [username],
+      (err, result) => {
+        if (err) {
+          throw err;
+        }
+        if (result.length > 0) {
+          console.log(result[0]);
+          bcrypt.compare(
+            oldPassowrd.toString(),
+            result[0].password,
+            (err, result) => {
+              if (err) {
+                throw err;
+              }
+              if (result) {
+                bcrypt.hash(newPassword.toString(), salt, (err, hash) => {
+                  if (err) {
+                    throw err;
+                  }
+                  if (hash) {
+                    pool.query(
+                      ServiceEmployee.rePassword,
+                      [hash, username],
+                      (err, data) => {
+                        if (err) {
+                          throw err;
+                        }
+                        if (data) {
+                          return res
+                            .status(200)
+                            .json({ message: "success", data: data });
+                        }
+                      }
+                    );
+                  }
+                });
+              }
+            }
+          );
+        } else {
+          return res.status(400).json({
+            message: "Sai password hoặc username! vui lòng kiểm tra lại",
+          });
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({ message: "fails" });
+  }
+};
+
+const updateInformation = (req, res) => {};
+
+module.exports = {
+  createEmployee,
+  loginEmployee,
+  getAllEmployee,
+  rePassword,
+  setNewPassword,
+  updateInformation,
+};
--- API/Employee/EmployeeRoutes.js
+++ API/Employee/EmployeeRoutes.js
@@ -1,10 +1,18 @@
-import { createEmployee, getAllEmployee, loginEmployee } from "./EmployeeController";
+import {
+  createEmployee,
+  getAllEmployee,
+  loginEmployee,
+  rePassword,
+  setNewPassword,
+} from "./EmployeeController";
 
 const express = require("express");
 const router = express.Router();
 export default function EmployeeRoutes(app) {
   router.post("/create", createEmployee);
-  router.get('/get-all',getAllEmployee)
+  router.get("/get-all", getAllEmployee);
   router.post("/login", loginEmployee);
+  router.put("/re-password", rePassword);
+  router.put("/renew-password", setNewPassword);
   return app.use("/employee", router);
 }
